f9f94f534e545b8957438c68c32794b8dbdfc087
ChillyRide <sebastianmars78@gmail.com>
2023-05-17 23:30:48 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { User } from 'components/profile/Profile';
 import { Data } from 'components/Statistics/Statistics'
 import { FriendList } from 'components/FriendList/FriendList'
+import { Transactions } from 'components/Transactions/Transactions'
+import transactions from 'json/transactions.json';
 import user from 'json/user.json';
 import data from 'json/data.json';
 import friends from 'json/friends.json';
@@ -22,6 +24,7 @@ export const App = () => {
       <User profile={user}/>
       <Data title="Upload stats" stats={data}/>
       <FriendList friends={friends}/>
+      {/* <Transactions/> */}
     </div>
   );
 };
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -16,3 +16,14 @@ export const FriendList = ({ friends }) => {
     </FriendListCSS>
   );
 };
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
