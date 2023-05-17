8a6f90af0c22475979657bd6f8b257a64a836ef2
ChillyRide <sebastianmars78@gmail.com>
2023-05-18 02:30:10 +0300

    up

--- src/components/Transactions/Trnsactions.styled.js
+++ src/components/Transactions/Trnsactions.styled.js
@@ -5,6 +5,7 @@ export const TableCSS = styled.table`
   height: auto;
   text-align: center;
   box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.15);
+  margin-bottom: 50px;
 `;
 
 export const TheadCSS = styled.thead`
